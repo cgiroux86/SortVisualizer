3190196dde6d0ba4aa768a53b2efdccd89fbbb0d
Chris Giroux <chris.giroux86@gmail.com>
2020-09-05 11:44:41 -0400

    beginning of merge-sort visualization

--- sort-viz/src/algorithms/mergeSort.js
+++ sort-viz/src/algorithms/mergeSort.js
@@ -1,17 +1,38 @@
-function mergeSort(arr) {
+const updates = [];
+export function mergeSort(
+  arr,
+  setArr,
+  setSorting,
+  funcObj,
+  currentSorted,
+  setCurrentSorted,
+  setSwapping,
+  swapping
+) {
+  let start = 0;
+  let end = arr.length - 1;
+  const results = _mergeSort(arr, updates, start, end);
+  console.log("funcObj", funcObj);
+
+  handleTimeout(funcObj, setSorting);
+}
+
+function _mergeSort(arr, updates, start, end) {
   if (arr.length > 1) {
     let mid = Math.floor(arr.length / 2);
     let left = arr.slice(0, mid);
     let right = arr.slice(mid, arr.length);
-    left = mergeSort(left);
-    right = mergeSort(right);
-    return merge(left, right);
+    let half = Math.floor((start + end) / 2);
+    left = _mergeSort(left, updates, 0, half);
+    right = _mergeSort(right, updates, half, arr.length);
+    return merge(left, right, start, end, updates);
   }
   return arr;
 }
-function merge(arr1, arr2) {
+function merge(arr1, arr2, start, end) {
   const arr = [];
   while (arr1.length && arr2.length) {
+    updates.push([arr1[0], arr2[0]]);
     if (arr1[0] <= arr2[0]) {
       arr.push(arr1.shift());
     } else {
@@ -19,7 +40,18 @@ function merge(arr1, arr2) {
     }
   }
   arr.push(...arr1, ...arr2);
+  updates.push([...arr]);
+  console.log(updates, arr);
   return arr;
 }
 
-console.log(mergeSort([5, 2, 1, 9, 3, 4, 8, 2, 6]));
+function handleTimeout(funcObj, setSorting) {
+  if (!updates.length) {
+    setSorting(false);
+    return;
+  }
+  funcObj.setArr(updates.shift());
+  setTimeout(() => {
+    handleTimeout(funcObj, setSorting);
+  }, 100);
+}
